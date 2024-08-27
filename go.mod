8841f66c184014786d87b015bd0276641a747fde
Mateus Felipe da Silveira VIeira <65318468+habdig7oficial@users.noreply.github.com>
2024-08-27 23:45:55 +0000

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module aula6/m/v2
+
+go 1.23.0
---
+++ trigger.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Funcionario struct {
+	nome string
+	salario float32
+}
+
+func main() {
+	funcionarios := []Funcionario {
+		Funcionario{
+           nome: "Mateus",
+		   salario: 100.00,
+	    },
+		Funcionario{
+			nome: "Marcos",
+			salario: 600.50,
+		},
+    }
+
+	var novo_salario []Funcionario = funcionarios
+	
+	for i := 0; i < len(funcionarios); i++ {
+		if funcionarios[i].salario <= 500 {
+			novo_salario[i].salario = funcionarios[i].salario + (funcionarios[i].salario / 100) * 20
+		} else {
+           novo_salario[i].salario = funcionarios[i].salario + (funcionarios[i].salario / 100) * 10
+		}
+	}
+
+	fmt.Println(novo_salario)
+}
